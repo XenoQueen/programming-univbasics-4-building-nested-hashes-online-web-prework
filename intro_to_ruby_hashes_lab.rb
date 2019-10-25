b827dda60c11baa51c28ed7c63ba60b9fdb3ea88
Maika Kailani <queenmab051@gmail.com>
2019-10-25 02:57:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,13 +31,30 @@ def monopoly_with_third_tier
     }
   }
   
-
- 
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+ galaxy_planets = {
+   :republic => {
+     "Ord Mantell" => {
+       :native_species => "Cathar",
+       :climate => "Tropical"
+     },
+      "Tython" => {
+        :native_species => "Flesh Raiders",
+        :climate => "Jungle"
+      }
+   },
+  :empire => {
+    "Hutta" => {
+      :native_species => "Evocii"
+      :climate => "Swamp"
+    },
+    "Korriban" => {
+      :native_species => "Sith"
+      :climate => "Desert"
+      }
+    }
+  }
+ 
 end
