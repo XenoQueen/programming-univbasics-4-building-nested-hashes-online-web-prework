217edfaf496658a25dae0dc12199ec72219ced57
Maika Kailani <queenmab051@gmail.com>
2019-10-25 02:46:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,8 @@ end
 def monopoly_with_second_tier
   galaxy_planets = {
    :republic => {
-     "", "Tython", :empire
+     "Ord Mantell", "Tython"}
+     :empire
    }
 
 def monopoly_with_third_tier
